b73317e7ef2edb010380c1afbf2a8457478a0aa8
Abhishek Majumdar <60106826+abmajum@users.noreply.github.com>
2025-07-04 13:30:29 +0530

    extract all the parameters, titiles links etc

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Setup Chrome options
 chrome_options = webdriver.ChromeOptions()
@@ -23,4 +26,53 @@ def get_torrents(encoded_query: str, page: str = 1):
   driver.get(f"{url}{final_encoded_query}/")
   # Wait until search results are loaded
   wait = WebDriverWait(driver, 10)
+  wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "cellMainLink")))
+
+  # --- Get pagination links ---
+  pagination_links = driver.find_elements(By.CSS_SELECTOR, "a.turnoverButton")
+  page_urls = set()
+  for link in pagination_links:
+      href = link.get_attribute("href")
+      if href:
+          page_urls.add(href.strip())
+  # print("\nAll Pagination Pages:")
+  # for page_url in sorted(page_urls):
+  #     print(page_url)
+  # Extract titles and links
+  results = {}
+  links = driver.find_elements(By.CLASS_NAME, "cellMainLink")
+  sizes = driver.find_elements(By.CSS_SELECTOR, "td.nobr.center")
+  uploaders = driver.find_elements(By.CSS_SELECTOR, "div.mainpart .plain")
+  list_of_uploader = []
+  for uploader in uploaders:
+      uploader_name = uploader.text.strip()
+      if 'results' not in uploader_name:
+          list_of_uploader.append(uploader_name)
+  list_of_uploader = [name.strip() for name in list_of_uploader if name.strip()]
+
+  print(list_of_uploader)
+  ages = driver.find_elements(By.CSS_SELECTOR, "td.center[title]")
+  seeds = driver.find_elements(By.CSS_SELECTOR, "td.green.center")
+
+  # for idx, link in enumerate(links, start=1):
+  #     title = link.text.strip()
+  #     page_url = link.get_attribute("href")
+  #     results[idx] = {"title": title, "page_url": page_url}
+  for idx, (link, size, uploader, age, seed) in enumerate(zip(links, sizes, list_of_uploader, ages, seeds), start=1):
+      title = link.text.strip()
+      page_url = link.get_attribute("href")
+      size_text = size.text.strip()
+      age_text = age.text.strip()
+      seed_text = seed.text.strip()
+
+      results[idx] = {
+          "title": title,
+          "page_url": page_url,
+          "size": size_text,
+          "uploader": uploader,
+          "age": age_text,
+          "seed": seed_text
+
+      }
   driver.quit()
+  return results, len(page_urls)
